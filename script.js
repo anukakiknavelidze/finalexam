54b9811d7e526a8fa2d1bf7d176172e57c409a0a
anukakiknavelidze <72189319+anukakiknavelidze@users.noreply.github.com>
2021-02-08 21:40:40 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,62 +1,55 @@
-let canvasHeight = 500
-let row
-let grid
-let amtOfHorizontalCircles = 15
-let linesAmt = 50
-let ySpacing = 10
-let lineSpacing = canvasHeight/linesAmt
+let symmetry = 6; /* ხაზის სისქე */
 
-const createCircle = (fillColor, x, y, maxRadius) => {
-    let xpos = x
-    let ypos = y
-    return {
-        xpos,
-        ypos,
-        display() {
-            noStroke()
-            fill(fillColor)
-            ellipse(xpos, ypos, maxRadius, maxRadius)
-        },
+let angle = 360 / symmetry; /* რკალის რაოდენობა */
+let saveButton, clearButton, mouseButton, keyboardButton; /* ღილაკები */
+let slider; /* ფუნჯის ზომის შეცვლა */
+
+function setup() {
+    createCanvas(1263, 510); /* ფონი */
+    angleMode(DEGREES); /* დახრილობა */
+    background('#fae'); /* background-ის ფერი */
+
+    saveButton = createButton('save'); /* შენახვის ფუნქცია */
+    saveButton.mousePressed(saveFile);/* მაუსის ღილაკით */
+
+    clearButton = createButton('clear'); /* ახალი ფურცელი */
+    clearButton.mousePressed(clearScreen); /* მაუსის ღილაკით */
+
+
+    brushSizeSlider = createButton('Brush Size Slider'); /* ფუნჯის ზომის რეგულირება */
+    sizeSlider = createSlider(1, 32, 4, 0.1);
+} /* ზომები */
+
+function saveFile() {
+    save('design.jpg');
+} /* შენახვის ფუნქცია pnj ფორმატში */
+
+function clearScreen() {
+    background('#fae'); /* background-ის ფერი ახალ ფურცელზე */
+}
+
+
+function draw() {
+    translate(width / 2, height / 2); /* ცენტრი */
+
+    if (mouseX > 0 && mouseX < width && mouseY > 0 && mouseY < height) {
+        let mx = mouseX - width / 2;
+        let my = mouseY - height / 2;
+        let pmx = pmouseX - width / 2;
+        let pmy = pmouseY - height / 2;
+        /* ხაზების დახრილობა, ალგორითმი */
+
+        if (mouseIsPressed) {
+            for (let i = 0; i < symmetry; i++) {
+                rotate(angle);
+                let sw = sizeSlider.value();
+                strokeWeight(sw);
+                line(mx, my, pmx, pmy);
+                push();
+                scale(1, -1);
+                line(mx, my, pmx, pmy);
+                pop();
+            } /* ცენტრის და ხაზის ცვლილებები */
+        }
     }
-}
-
-const createLine = (arr, amt,x, y) => {
-    if( arr.length < amt) {
-        arr.push(createCircle(`rgba(0, 0, 0, ${Math.random().toFixed(1)})`, x,  y, Math.random()*9))
-        createLine(arr, amt, x+=width/amtOfHorizontalCircles, y)
-    }
-    return arr
-}
-
-
-const createGrid = (arr, linesAmt, y) => {
-    if( arr.length < linesAmt) {
-        arr.push(createLine([], amtOfHorizontalCircles, 0, y))
-        createGrid(arr, linesAmt, y += lineSpacing)}
-    return arr
-}
-
-const slideRow = () => {
-    grid.forEach( (line) => {
-        line.forEach( x => {
-            x.slide(ySpacing)
-        })
-    })
-}
-
-
-function setup () {
-    createCanvas(600, canvasHeight)
-    background("#255")
-    grid = createGrid([], linesAmt, 10)
-    setInterval(slideRow, 1000)
-}
-
-function draw () {
-    background("255")
-    grid.forEach( row => {
-        row.forEach( x => {
-            x.display()
-        })
-    })
 }
